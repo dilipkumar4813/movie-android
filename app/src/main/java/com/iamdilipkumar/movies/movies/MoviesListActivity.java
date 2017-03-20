6d1d7736730dc976582e0721af55a58b4cd2517c
Dilip Kumar <dilipkumar4813@gmail.com>
2017-03-20 22:28:21 +0530

    Connect Network utility and create async task for threading

--- app/src/main/java/com/iamdilipkumar/movies/movies/MoviesListActivity.java
+++ app/src/main/java/com/iamdilipkumar/movies/movies/MoviesListActivity.java
@@ -1,21 +1,22 @@
 package com.iamdilipkumar.movies.movies;
 
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.widget.ProgressBar;
+
+import com.iamdilipkumar.movies.movies.utilities.NetworkUtils;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
+ * Launcher activity that shows movies in grid layout
  * Created on 20/03/2017
+ *
  * @author dilipkumar4813
  * @version 1.0
+ *
  */
 
 public class MoviesListActivity extends AppCompatActivity {
@@ -28,5 +29,40 @@ public class MoviesListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movies_list);
 
         mLoading = (ProgressBar) findViewById(R.id.pb_loading_data);
+
+        getMoviesList(getString(R.string.sort_popular));
+    }
+
+    /**
+     * Method to build the URL and call the API
+     * Constructs the URL (using {@link NetworkUtils})
+     *
+     * @param sortOrder the order in which movies have to be sorted
+     */
+    private void getMoviesList(String sortOrder) {
+        try {
+            URL url = NetworkUtils.buildUrl(sortOrder);
+            new MoviesQueryTask().execute(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class MoviesQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
