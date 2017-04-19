b88988d7ca49f1442a651a6835f2b00cce513d01
Dilip Kumar <dilipkumar4813@gmail.com>
2017-04-20 00:25:46 +0530

    Styling the detail page

--- app/src/main/java/com/iamdilipkumar/movies/movies/utilities/ShareUtils.java
+++ app/src/main/java/com/iamdilipkumar/movies/movies/utilities/ShareUtils.java
@@ -1,7 +1,6 @@
 package com.iamdilipkumar.movies.movies.utilities;
 
 import android.app.Activity;
-import android.content.Context;
 import android.support.v4.app.ShareCompat;
 
 import com.iamdilipkumar.movies.movies.R;
